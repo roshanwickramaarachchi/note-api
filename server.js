8cb3e2c4241532fa61fe3fe9d3c65086aed21fd3
roshan <wickramaarachchiroshan@gmail.com>
2021-09-01 17:33:55 +0530

    connect database

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //load env var
 dotenv.config({path: './config/config.env'})
 
+// Connect to database
+connectDB();
+
 const app= express()
 
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
+  ),
+);
