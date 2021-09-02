60bb7a5c4cf633a6de38992457accc477c2b0275
roshan <wickramaarachchiroshan@gmail.com>
2021-09-02 13:17:16 +0530

    complete error handle

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 
 //load env var
 dotenv.config({path: './config/config.env'})
@@ -17,9 +19,16 @@ const app= express()
 // Body parser
 app.use(express.json({ limit: '15mb' })); 
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Mount routers
 app.use('/api/v1/notes', notes)
 
+// my-error hadle
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
