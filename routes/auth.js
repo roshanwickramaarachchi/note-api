429fa7f9a01d1734edfbb79dc2b03c228e3731f4
roshan <wickramaarachchiroshan@gmail.com>
2021-09-03 23:04:11 +0530

    finish, password forgot

--- routes/auth.js
+++ routes/auth.js
@@ -3,19 +3,21 @@ const {
     register,
     login,
     getMe,
+    forgotPassword,
+    resetPassword,
 } = require('../controllers/auth')
 
 const router = express.Router()
 
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/auth')
 
 router.post('/register', register)
 router.post('/login', login)
-// router.get('/logout', logout);
-router.get('/me', protect, getMe);
-// router.put('/updateDetails', protect, updateDetails);
-// router.put('/updatepassword', protect, updatePassword);
-// router.post('/forgotPassword', forgotPassword);
-// router.put('/resetPassword/:resettoken', resetPassword);
+// router.get('/logout', logout)
+router.get('/me', protect, getMe)
+// router.put('/updateDetails', protect, updateDetails)
+// router.put('/updatepassword', protect, updatePassword)
+router.post('/forgotPassword', forgotPassword)
+router.put('/resetPassword/:resettoken', resetPassword)
 
 module.exports = router
