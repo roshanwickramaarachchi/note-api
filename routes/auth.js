bb9ae56ca25da6cde9222aac69d3cf1856ec9d24
roshan <wickramaarachchiroshan@gmail.com>
2021-09-04 10:04:32 +0530

    finished authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const {
     getMe,
     forgotPassword,
     resetPassword,
+    updateDetails,
+    updatePassword,
 } = require('../controllers/auth')
 
 const router = express.Router()
@@ -15,8 +17,8 @@ router.post('/register', register)
 router.post('/login', login)
 // router.get('/logout', logout)
 router.get('/me', protect, getMe)
-// router.put('/updateDetails', protect, updateDetails)
-// router.put('/updatepassword', protect, updatePassword)
+router.put('/updateDetails', protect, updateDetails)
+router.put('/updatepassword', protect, updatePassword)
 router.post('/forgotPassword', forgotPassword)
 router.put('/resetPassword/:resettoken', resetPassword)
 
